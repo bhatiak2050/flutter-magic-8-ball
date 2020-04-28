94dcb64b11723fada96d0083d5ac771da3fa8918
bhatiak2050 <karanbhatia2050@gmail.com>
2020-04-28 18:00:21 +0530

    add stateless widget w appbar n body

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Container(),
+    );
+  }
+}
