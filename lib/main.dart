a769096dac768eb98e7c6ebc91a5eede465d1549
bhatiak2050 <karanbhatia2050@gmail.com>
2020-04-28 18:02:39 +0530

    add stateful widget w image

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,21 @@ class BallPage extends StatelessWidget {
         title: Text('Ask Me Anything'),
         backgroundColor: Colors.blue[900],
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Image.asset('images/ball1.png')),
     );
   }
 }
